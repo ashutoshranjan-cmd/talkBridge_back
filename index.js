00b53aadb89cb2bc69e6d2d045ba39f7193b6270
Ashutosh Ranjan <72221793+ashutoshranjan-cmd@users.noreply.github.com>
2024-11-03 20:08:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors({
 // routes
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
-app.get('/temp',(req,res)=>{
+app.get('/',(req,res)=>{
     res.send("this is working")
 })
 
